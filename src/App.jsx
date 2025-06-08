0664e3a2aef773e3bdde604d2779ed317ef45be8
xnu05x <114028215+xnu05x@users.noreply.github.com>
2025-06-08 16:34:16 +0530

    Lesson 429 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import UserProfile from "./components/userprofile/userprofile"
 
 
+const user = {
+  firstName: "Sam",
+  lastName: "Singh",
+  skills: "React JS"
+}
+
+const user2 = {
+  firstName: "Samantha",
+  lastName: "Ruth",
+  skills: "Node JS"
+}
+
+
+
+
+
+
+
 function App() {
 
   return (
     <>
-      <UserProfile />
+      <UserProfile firstName={user.firstName} lastName={user.lastName} skills={user.skills} />
+      <UserProfile firstName={user2.firstName} skills={user2.skills}/>
       <h1>Hello world</h1>
     </>
   )
--- src/components/userprofile/userprofile.jsx
+++ src/components/userprofile/userprofile.jsx
@@ -2,14 +2,18 @@
 import styles from "./userprofile.module.css"
 
 
-export default function UserProfile(){
+
+export default function UserProfile(props){
+
+  const { firstName ="Not added", lastName = "Not added", skills = "Not added" } = props;
+
   return (
   <>
     <section>
       <ul className={`${styles.userDetails} ${styles.border}`}>
-        <li>First Name: John</li>
-        <li>Last Name: Doe</li>
-        <li>Skills: React, JavasScript</li>
+        <li>First Name: {firstName}</li>
+        <li>Last Name: {lastName}</li>
+        <li>Skills: {skills}</li>
       </ul>
     </section>
   </>
